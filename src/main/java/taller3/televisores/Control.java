a643e040b4f7a99133f78e82b817fd9862bcba50
melbulaf <mebulaf@unal.edu.co>
2024-11-18 19:18:20 -0500

    Prueba 7
    Probando el código.

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -20,28 +20,28 @@ public class Control {
 	}
 	public void canalUp() {
 		if (tv.estado == true) {
-			if (tv.canal >= 1 && tv.canal <= 120){
+			if (tv.canal > 1 && tv.canal < 120){
 				tv.canal++;
 			}
 		}
 	}
 	public void canalDown() {
 		if (tv.estado == true) {
-			if (tv.canal >= 1 && tv.canal <= 120){
+			if (tv.canal > 1 && tv.canal < 120){
 				tv.canal--;
 			}
 		}
 	}
 	public void volumenUp() {
 		if (tv.estado == true) {
-			if (tv.volumen >= 0 && tv.volumen <= 7){
+			if (tv.volumen > 0 && tv.volumen < 7){
 				tv.volumen++;
 			}
 		}
 	}
 	public void volumenDown() {
 		if (tv.estado == true) {
-			if (tv.volumen >= 0 && tv.volumen <= 7){
+			if (tv.volumen > 0 && tv.volumen < 7){
 				tv.volumen--;
 			}
 		}
